e462b49725cef344138cd3aa77b878973b36647c
Suraphel <suraphelm@q-free.com>
2022-08-29 22:51:03 +0200

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// here we are using two base images to create this application
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+  
+        <h1>Served by dockerized nginx web server along with node  </h1>
+  
+    </div>
+  );
+}
+
+export default App;
